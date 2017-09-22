62682f8e5df6b1e89ed0a2eda6a561c0339fb49c
HiImChubs <31480625+HiImChubs@users.noreply.github.com>
2017-09-22 10:40:35 -0400

    Add files via upload
    added start and end text

--- guess_a_number.py
+++ guess_a_number.py
@@ -1,58 +1,95 @@
 import random
+import math
 
-#config
-high= 100
-low= 10
+# config
+low = 0
+high = 20
+limit = math.ceil(math.log(high, 2))
+
 
 # helper functions
+def show_start_screen():
+        print("""
+     _____                                                     _               
+    |  __ \                                                   | |              
+    | |  \/_   _  ___  ___ ___    __ _   _ __  _   _ _ __ ___ | |__   ___ _ __ 
+    | | __| | | |/ _ \/ __/ __|  / _` | | '_ \| | | | '_ ` _ \| '_ \ / _ \ '__|
+    | |_\ \ |_| |  __/\__ \__ \ | (_| | | | | | |_| | | | | | | |_) |  __/ |   
+     \____/\__,_|\___||___/___/  \__,_| |_| |_|\__,_|_| |_| |_|_.__/ \___|_|   
+                                                                               
+                                                                               """)
+
+def show_credits():
+    print("""
+ _   __    _____                _____           
+| | / /   | ___ \              |  _  \          
+| |/ /    | |_/ /_   _  ___    | | | |___ _ __  
+|    \    | ___ \ | | |/ _ \   | | | / _ \ '_ \ 
+| |\  \   | |_/ / |_| |  __/   | |/ /  __/ | | |
+\_| \_/   \____/ \__, |\___|   |___/ \___|_| |_|
+                  __/ |                         
+                 |___/                          """)
+    
 def get_guess():
     while True:
-        guess = input("Guess a number:")
+        guess = input("Guess a number: ")
 
         if guess.isnumeric():
             guess = int(guess)
             return guess
         else:
-            print("You must enter a number")
-            
+            print("You must enter a number.")
+
+def pick_number():
+    print("I'm thinking of a number from " + str(low) + " to " + str(high) +".")
+
+    return random.randint(low, high)
+
+def check_guess(guess, rand):
+    if guess < rand:
+        print("You guessed too low.")
+    elif guess > rand:
+        print("You guessed too high.")
+
+def show_result(guess, rand):
+    if guess == rand:
+        print("You win!")
+    else:
+        print("You are such a loser! The number was " + str(rand) + ".")
+
 def play_again():
     while True:
-        decision = input("You wanna play again? (y/n) ")
+        decision = input("Would you like to play again? (y/n) ")
 
         if decision == 'y' or decision == 'yes':
             return True
         elif decision == 'n' or decision == 'no':
             return False
         else:
-            print("Bruh just type y or n it isn't that hard...")
-again = True
+            print("I don't understand. Please enter 'y' or 'n'.")
 
-while again:           
-    #start game
-    rand = random.randint(1, 100)
-    print("I'm thinking of a number from " + str(low) + " to " + str(high) + " .");
-
+def play():
     guess = -1
     tries = 0
-    limit = 10
 
-    #play game
+    rand = pick_number()
+    
     while guess != rand and tries < limit:
         guess = get_guess()
-        
-        if guess < rand:
-            print("You guessed too low.")
-        elif guess > rand:
-            print("You guessed too high.")
-            
+        check_guess(guess, rand)
+
         tries += 1
 
-    #game over
-    if guess == rand:
-        print("Wow great you can guess a number")
-    else:
-        print("How are you this bad at a simple game... the number was " + str(rand) + ".")
+    show_result(guess, rand)
 
-    again = play_again()
 
-print("Later Nerd")
+# Game starts running here
+show_start_screen()
+
+playing = True
+
+while playing:
+    play()
+    playing = play_again()
+
+show_credits()
